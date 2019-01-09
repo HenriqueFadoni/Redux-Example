141785c7d28bd3956e0b5f4f742e3d39f970cf76
Henrique Fadoni <henriquefadonii@gmail.com>
2019-01-09 11:06:17 -0200

    A Linear Switch Case Statement

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -5,13 +5,16 @@ const initialState = {
     results: []
 }
 
+const deleteResults = (state, action) => {
+    const updatedArray = state.results.filter( results => results.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray} );
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.STORE_RESULTS:
+        case actionTypes.STORE_RESULTS: 
             return updateObject(state, { results: state.results.concat({id: new Date(), value: action.result}) });
-        case actionTypes.DELETE_RESULTS:
-            const updatedArray = state.results.filter( results => results.id !== action.resultElId);
-            return updateObject(state, {results: updatedArray} );
+        case actionTypes.DELETE_RESULTS: return deleteResults(state, action);
     }
     return state;
 }
